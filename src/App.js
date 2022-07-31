1e4d7c1334281d9aed67a24e5caf2d09e894999c
salvadorperezm <salvadorperezm115@gmail.com>
2022-07-31 10:15:07 -0600

    fetch to api done

--- src/App.js
+++ src/App.js
@@ -1,85 +1,50 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import { SimpleGrid, Button } from "@chakra-ui/react";
 import Card from "./Card";
+const axios = require("axios");
 
 function App() {
-  const users = [
-    {
-      id: 1,
-      name: "Salvador",
-      profession: "Software Developer",
-      email: "salvador@mail.com",
-      location: "Managua, Nicaragua",
-    },
-    {
-      id: 2,
-      name: "Salvador",
-      profession: "Software Developer",
-      email: "salvador@mail.com",
-      location: "Managua, Nicaragua",
-    },
-    {
-      id: 3,
-      name: "Salvador",
-      profession: "Software Developer",
-      email: "salvador@mail.com",
-      location: "Managua, Nicaragua",
-    },
-    {
-      id: 4,
-      name: "Salvador",
-      profession: "Software Developer",
-      email: "salvador@mail.com",
-      location: "Managua, Nicaragua",
-    },
-    {
-      id: 5,
-      name: "Salvador",
-      profession: "Software Developer",
-      email: "salvador@mail.com",
-      location: "Managua, Nicaragua",
-    },
-    {
-      id: 6,
-      name: "Salvador",
-      profession: "Software Developer",
-      email: "salvador@mail.com",
-      location: "Managua, Nicaragua",
-    },
-    {
-      id: 7,
-      name: "Salvador",
-      profession: "Software Developer",
-      email: "salvador@mail.com",
-      location: "Managua, Nicaragua",
-    },
-    {
-      id: 8,
-      name: "Salvador",
-      profession: "Software Developer",
-      email: "salvador@mail.com",
-      location: "Managua, Nicaragua",
-    },
-    {
-      id: 9,
-      name: "Salvador",
-      profession: "Software Developer",
-      email: "salvador@mail.com",
-      location: "Managua, Nicaragua",
-    },
-    {
-      id: 10,
-      name: "Salvador",
-      profession: "Software Developer",
-      email: "salvador@mail.com",
-      location: "Managua, Nicaragua",
-    },
-  ];
+  const defaultUser = {
+    id: "19f09h1ft",
+    name: "John Doe",
+    profession: "Not Available",
+    email: "johndoe@email.com",
+    location: "New York, NY",
+  };
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://random-data-api.com/api/users/random_user?size=10")
+      .then((res) => {
+        setUsers(res.data);
+      })
+      .catch((error) => {
+        setUsers(defaultUser);
+      });
+  }, []);
+
   return (
     <main className="main-container">
       <div className="main-container__text">
         <h1 className="main-container__title">React API Characters</h1>
-        <Button colorScheme="blue">New Characters</Button>
+        <Button
+          colorScheme="blue"
+          onClick={() => {
+            axios
+              .get("https://random-data-api.com/api/users/random_user?size=10")
+              .then((res) => {
+                setUsers(res.data);
+              })
+              .catch((error) => {
+                setUsers(defaultUser);
+              });
+          }}
+        >
+          New Characters
+        </Button>
       </div>
       <SimpleGrid
         minChildWidth="200px"
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import "./Card.css";
-import dummyImage from "./dummy-image.png";
 
 const Card = ({ props }) => {
   return (
     <div className="scene scene--card">
       <div className="card">
         <div className="card__face card__face--front">
-          <img src={dummyImage} className="avatar" alt="avatar"></img>
-          <h3>{props.name}</h3>
-          <p>{props.profession}</p>
+          <img src={`${props.avatar}`} className="avatar" alt="avatar"></img>
         </div>
         <div className="card__face card__face--back">
+          <h3 className="card__title">{`${props.first_name} ${props.last_name}`}</h3>
+          <p>{props.employment.title}</p>
           <p>{props.email}</p>
-          <p>{props.location}</p>
+          <p>{`${props.address.state}, ${props.address.country}.`}</p>
         </div>
       </div>
     </div>
