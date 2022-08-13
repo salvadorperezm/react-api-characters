3515e163cd79b0b765d48f95c2abb0c704b6d664
salvadorperezm <salvadorperezm115@gmail.com>
2022-08-13 16:32:30 -0600

    adding eslint and prettier to project

--- src/App.js
+++ src/App.js
@@ -1,53 +1,53 @@
-import "./App.css";
-import { useState, useEffect } from "react";
-import { SimpleGrid } from "@chakra-ui/react";
-import Card from "./Card";
-const axios = require("axios");
+import './App.css'
+import { useState, useEffect } from 'react'
+import { SimpleGrid } from '@chakra-ui/react'
+import Card from './Card'
+const axios = require('axios')
 
 function App() {
   const defaultUser = {
-    id: "19f09h1ft",
-    name: "John Doe",
-    profession: "Not Available",
-    email: "johndoe@email.com",
-    location: "New York, NY",
-  };
+    id: '19f09h1ft',
+    name: 'John Doe',
+    profession: 'Not Available',
+    email: 'johndoe@email.com',
+    location: 'New York, NY',
+  }
 
-  const [users, setUsers] = useState([]);
-  const [filteredUsers, setFilteredUsers] = useState("");
+  const [users, setUsers] = useState([])
+  const [filteredUsers, setFilteredUsers] = useState('')
 
   function getData() {
     axios
-      .get("https://rickandmortyapi.com/api/character")
+      .get(process.env.REACT_APP_PUBLIC_URL)
       .then((res) => {
-        console.log(res.data.results);
-        setUsers(res.data.results);
+        console.log(res.data.results)
+        setUsers(res.data.results)
       })
       .catch(() => {
-        setUsers(defaultUser);
-      });
+        setUsers(defaultUser)
+      })
   }
 
   useEffect(() => {
-    getData();
-  }, []);
+    getData()
+  }, [])
 
   return (
-    <main className="main-container">
-      <div className="main-container__text">
-        <h1 className="main-container__title">Rick and Morty Characters</h1>
+    <main className='main-container'>
+      <div className='main-container__text'>
+        <h1 className='main-container__title'>Rick and Morty Characters</h1>
       </div>
       <SimpleGrid
-        minChildWidth="200px"
-        className="main-container__grid"
-        gap="20px"
+        minChildWidth='200px'
+        className='main-container__grid'
+        gap='20px'
       >
         {users.map((user) => {
-          return <Card props={user} key={user.id} />;
+          return <Card props={user} key={user.id} />
         })}
       </SimpleGrid>
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,22 +1,21 @@
-import React from "react";
-import "./Card.css";
+import './Card.css'
 
 const Card = ({ props }) => {
   return (
-    <div className="scene scene--card">
-      <div className="card">
-        <div className="card__face card__face--front">
-          <img src={`${props.image}`} className="avatar" alt="avatar"></img>
+    <div className='scene scene--card'>
+      <div className='card'>
+        <div className='card__face card__face--front'>
+          <img src={`${props.image}`} className='avatar' alt='avatar'></img>
         </div>
-        <div className="card__face card__face--back">
-          <h3 className="card__title">{`${props.name}`}</h3>
+        <div className='card__face card__face--back'>
+          <h3 className='card__title'>{`${props.name}`}</h3>
           <p>{`Status: ${props.status}`}</p>
           <p>{`Species: ${props.species}`}</p>
           <p>{`Location: ${props.location.name}.`}</p>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Card;
+export default Card
