6da1320c6b610fd069b21adb78e78c9e8a3515a6
salvadorperezm <salvadorperezm115@gmail.com>
2022-08-16 22:23:07 -0600

    modal component updated

--- src/components/CardLocation/CardLocation.jsx
+++ src/components/CardLocation/CardLocation.jsx
@@ -1,31 +1,98 @@
-import { useState } from 'react'
-import { ModalLocation } from '../Modal/ModalLocation'
-import { Button } from '@chakra-ui/react'
+import { useState, useEffect } from 'react'
+
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react'
 import './CardLocation.css'
+import axios from 'axios'
+import { Card } from '../Card/Card'
 
 const CardLocation = ({ location }) => {
-  const [showModal, setShowModal] = useState(false)
+  const [open, setOpen] = useState(false)
+  const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    async function getFetch() {
+      const result = await getUrls()
+      setCharacters(result)
+    }
+
+    getFetch()
+  }, [location.residents])
+
+  async function getUrls() {
+    const map = await Promise.all(
+      location.residents.map((resident) => {
+        return getData(resident)
+      })
+    )
+
+    return map
+  }
+
+  async function getData(url) {
+    try {
+      const { data } = await axios.get(`${url}`)
+      return data
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   return (
-    <div className='card-location__container'>
-      <img
-        className='card-location__image'
-        src='https://c4.wallpaperflare.com/wallpaper/229/145/81/rick-and-morty-wallpaper-preview.jpg'
-      />
-      <div className='card-location__text'>
-        <h1>{location.name}</h1>
-        <p>{location.type}</p>
-        <p>{location.dimension}</p>
-        <Button
-          onClick={() => {
-            setShowModal((state) => !state)
-          }}
-        >
-          Show {showModal ? `less` : `more`}
-        </Button>
+    <>
+      <div className='card-location__container'>
+        <img
+          className='card-location__image'
+          src='https://c4.wallpaperflare.com/wallpaper/229/145/81/rick-and-morty-wallpaper-preview.jpg'
+        />
+        <div className='card-location__text'>
+          <Button
+            className='card-location__button'
+            onClick={() => setOpen(true)}
+          >
+            Residents
+          </Button>
+          <h1>{location.name}</h1>
+          <p>{location.type}</p>
+          <p>{location.dimension}</p>
+        </div>
       </div>
-      {showModal && <ModalLocation residents={location.residents} />}
-    </div>
+      <Modal isOpen={open} onClose={() => setOpen(false)}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Characters that live here</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody
+            display={'flex'}
+            flexDirection={'column'}
+            alignItems={'center'}
+            gap={'10px'}
+          >
+            {characters.length !== 0 ? (
+              characters.map((character) => {
+                return <Card key={character.id} props={character} />
+              })
+            ) : (
+              <h1>This place in inhabited</h1>
+            )}
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={() => setOpen(false)}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   )
 }
 
